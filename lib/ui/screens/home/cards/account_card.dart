9f984e09a09813240a07e4dec1ac8c8be5d2d8ac
ricardodalarme <ricardoweasley@hotmail.com>
2021-06-09 10:49:41 -0300

    Animação ao abrir a conta

--- lib/ui/screens/home/cards/account_card.dart
+++ lib/ui/screens/home/cards/account_card.dart
@@ -4,6 +4,7 @@ import 'package:nubank_clone/ui/screens/account/account_screen.dart';
 import 'package:nubank_clone/ui/screens/home/components/main_card.dart';
 import 'package:nubank_clone/ui/theme/icons.dart';
 import 'package:nubank_clone/core/constants.dart';
+import 'package:page_transition/page_transition.dart';
 
 class AccountCard extends StatelessWidget {
   @override
@@ -19,7 +20,33 @@ class AccountCard extends StatelessWidget {
         SizedBox(height: 13),
         Text('R\$ $kBalance', style: Theme.of(context).textTheme.headline5),
       ],
-      onTap: () => MyRouter.pushPage(context, AccountScreen()),
+      onTap: () => Navigator.push(
+          context,
+          PageTransition(
+              type: PageTransitionType.rightToLeft,
+              duration: Duration(milliseconds: 400),
+              child: AccountScreen())),
     );
   }
+}
+
+class SlideRightRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideRightRoute(this.page)
+      : super(
+            pageBuilder: (
+              context,
+              animation,
+              secondaryAnimation,
+            ) =>
+                page,
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) =>
+                    SlideTransition(
+                      position: Tween<Offset>(
+                        begin: const Offset(-1, 0),
+                        end: Offset.zero,
+                      ).animate(animation),
+                      child: child,
+                    ));
 }
