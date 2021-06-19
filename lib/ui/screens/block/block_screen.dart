fcacdf693aad153f441fcc2b1be2699dbafa3da4
ricardodalarme <ricardoweasley@hotmail.com>
2021-06-19 00:13:23 -0300

    Ajustadp a forma como a bottomSheet de bloqueio do cartão ajusta o tamanho

--- lib/ui/screens/block/block_screen.dart
+++ lib/ui/screens/block/block_screen.dart
@@ -9,52 +9,49 @@ class BlockScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Wrap(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Column(
-            children: [
-              SizedBox(height: 40),
-              Icon(NuIcons.ic_virtual_card_blocked, color: kSecondaryTextColor),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    text: 'Você quer ',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2!
-                        .copyWith(height: 1.8),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'bloquear temporariamente ',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      TextSpan(text: 'o seu cartão?'),
-                    ],
+          SizedBox(height: 40),
+          Icon(NuIcons.ic_virtual_card_blocked, color: kSecondaryTextColor),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: 'Você quer ',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText2!
+                    .copyWith(height: 1.8),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'bloquear temporariamente ',
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                ),
-              ),
-              SizedBox(height: 40),
-              Container(height: 1, color: kLineColor),
-              Row(
-                children: [
-                  ExpandedButton(
-                    'Cancelar',
-                    onTap: () => MyRouter.popPage(context),
-                    color: kSecondaryTextColor,
-                  ),
-                  Container(
-                      margin: EdgeInsets.symmetric(vertical: 1),
-                      width: 1,
-                      height: 62,
-                      color: kLineColor),
-                  ExpandedButton(
-                    'Bloquear',
-                    color: kPrimaryColor,
-                  ),
+                  TextSpan(text: 'o seu cartão?'),
                 ],
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+          Container(height: 1, color: kLineColor),
+          Row(
+            children: [
+              ExpandedButton(
+                'Cancelar',
+                onTap: () => MyRouter.popPage(context),
+                color: kSecondaryTextColor,
+              ),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 1),
+                  width: 1,
+                  height: 62,
+                  color: kLineColor),
+              ExpandedButton(
+                'Bloquear',
+                color: kPrimaryColor,
               ),
             ],
           ),
