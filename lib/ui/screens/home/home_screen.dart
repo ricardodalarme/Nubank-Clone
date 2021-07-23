83d641de1beb759dd191ba989b3ce5a35696f8b1
ricardodalarme <ricardoweasley@hotmail.com>
2021-07-23 12:45:28 -0300

    Internacionalização na tela inicial

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:nubank_clone/core/app_state.dart';
 import 'package:nubank_clone/core/constants.dart';
+import 'package:nubank_clone/core/locale_keys.g.dart';
 import 'package:nubank_clone/core/router.dart';
 import 'package:nubank_clone/ui/screens/block/block_screen.dart';
 import 'package:nubank_clone/ui/screens/charge/charge_screen.dart';
@@ -58,7 +60,7 @@ class HomeScreen extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
-                            'Olá, $kUsername',
+                            LocaleKeys.hello.tr(args: [kUsername]),
                             style: Theme.of(context)
                                 .textTheme
                                 .headline5!
@@ -97,37 +99,37 @@ class HomeScreen extends StatelessWidget {
                   scrollDirection: Axis.horizontal,
                   child: Row(children: [
                     MenuButton(
-                      'Pix',
+                      LocaleKeys.pix.tr(),
                       NuIcons.rewards_ic_empty_state_other,
                       onTap: () =>
                           MyRouter.showBottomSheet(context, PixScreen()),
                     ),
                     MenuButton(
-                      'Pagar',
+                      LocaleKeys.pay.tr(),
                       NuIcons.ic_savings_global_action_pay,
                       onTap: () =>
                           MyRouter.showBottomSheet(context, PaymentScreen()),
                     ),
                     MenuButton(
-                      'Indicar amigos',
+                      LocaleKeys.refer_friend.tr(),
                       NuIcons.ic_refer_friend,
                       onTap: () =>
                           MyRouter.showBottomSheet(context, ReferScreen()),
                     ),
                     MenuButton(
-                      'Transferir',
+                      LocaleKeys.transfer.tr(),
                       NuIcons.ic_savings_global_action_transfer_out,
                       onTap: () =>
                           MyRouter.showBottomSheet(context, TransferScreen()),
                     ),
                     MenuButton(
-                      'Depositar',
+                      LocaleKeys.deposit.tr(),
                       NuIcons.ic_savings_global_action_transfer_in,
                       onTap: () =>
                           MyRouter.showBottomSheet(context, DepositScreen()),
                     ),
                     MenuButton(
-                      'Empréstimos',
+                      LocaleKeys.loan.tr(),
                       NuIcons.nuds_ic_personal_loan,
                       onTap: () => Navigator.push(
                           context,
@@ -136,14 +138,22 @@ class HomeScreen extends StatelessWidget {
                               duration: Duration(milliseconds: 400),
                               child: LoanScreen())),
                     ),
-                    MenuButton('Cartão virtual', NuIcons.ic_virtual_card),
-                    MenuButton('Recarga de celular', NuIcons.ic_phone,
-                        onTap: () => MyRouter.showBottomSheet(
-                            context, RechargeScreen())),
                     MenuButton(
-                        'Ajustar limite', NuIcons.cc_ic_limit_adjustment),
+                      LocaleKeys.virtual_card.tr(),
+                      NuIcons.ic_virtual_card,
+                    ),
                     MenuButton(
-                      'Bloquear cartão',
+                      LocaleKeys.recharge_phone.tr(),
+                      NuIcons.ic_phone,
+                      onTap: () =>
+                          MyRouter.showBottomSheet(context, RechargeScreen()),
+                    ),
+                    MenuButton(
+                      LocaleKeys.adjust_limit.tr(),
+                      NuIcons.cc_ic_limit_adjustment,
+                    ),
+                    MenuButton(
+                      LocaleKeys.block_card.tr(),
                       NuIcons.ic_virtual_card_blocked,
                       onTap: () => MyRouter.showBottomSheet(
                         context,
@@ -152,13 +162,19 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                     MenuButton(
-                      'Cobrar',
+                      LocaleKeys.charge.tr(),
                       NuIcons.nuds_ic_request_money,
                       onTap: () =>
                           MyRouter.showBottomSheet(context, ChargeScreen()),
                     ),
-                    MenuButton('Doação', NuIcons.nuds_ic_personal_loan),
-                    MenuButton('Me ajuda', NuIcons.help),
+                    MenuButton(
+                      LocaleKeys.donation.tr(),
+                      NuIcons.nuds_ic_personal_loan,
+                    ),
+                    MenuButton(
+                      LocaleKeys.help_me.tr(),
+                      NuIcons.help,
+                    ),
                     SizedBox(width: 8),
                   ]),
                 ),
