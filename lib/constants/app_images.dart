a24be3fc0764f33f63b0134bd9b25b7026540c2e
Ricardo Dalarme <ricardo.dalarme@profusion.mobi>
2022-10-29 13:35:34 -0300

    refactor(images): move the logos to the images folder

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -1,5 +1,9 @@
 abstract class AppImages {
-  static const String easyinvest = 'assets/images/easyinvest.jpg';
-  static const String pix = 'assets/images/pix.jpg';
-  static const String referFriend = 'assets/images/refer_friend.jpg';
+  static const String _path = 'assets/images/';
+  static const String logo = '${_path}logo.png';
+  static const String logoBackground = '${_path}logo_background.png';
+  static const String logoWhite = '${_path}logo_white.png';
+  static const String easyinvest = '${_path}easyinvest.jpg';
+  static const String pix = '${_path}pix.jpg';
+  static const String referFriend = '${_path}refer_friend.jpg';
 }
